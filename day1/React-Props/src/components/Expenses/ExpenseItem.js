0a4de6b8971ccaafe97d19159d241c0926a99f0a
carles espunya <carlespuna@gmail.com>
2022-11-14 11:38:27 +0100

    Day 1

---
+++ day1/React-Props/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+
+function ExpenseItem(props) {
+  const { date, title, amount} = props
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}€</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem;
